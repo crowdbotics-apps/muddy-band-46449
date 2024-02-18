10076a119b57c297baed022893f0f6deb34a5790
Crowdbotics <team@crowdbotics.com>
2024-02-18 04:19:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muddy-band-46449",
+  url: "https://muddy-band-46449.botics.co",
+  api: "https://muddy-band-46449.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
